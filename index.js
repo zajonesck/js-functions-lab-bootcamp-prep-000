2ad07aa332f6f54a15f0d77a7c9125b6c571d86b
Zachary Clinton Jones <zajonesck83@gmail.com>
2017-12-13 15:58:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ function happyHolidays(string) {
 function happyHolidaysTo(name) {
   return (`Happy holidays, ${name}!`)
 }
+function happyHolidayTo(holiday, name){
+  return (`Happy ${holiday}, ${name}!`)
+}
